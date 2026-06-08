0bae25e499c44c3e8870ef8ecb17fd46e318ca4d
pixelgamesite-maker <pixelgamesite@gmail.com>
2026-06-08 08:43:00 +0100

    Update assets.ts

--- client/src/lib/assets.ts
+++ client/src/lib/assets.ts
@@ -20,10 +20,6 @@ export const HONORARIES = [
   { name: 'TMA',     handle: '@tma_420',       url: `${BASE}/One/TMA.JPG` },
   { name: 'Brated',  handle: '@br4ted',        url: `${BASE}/One/Brated.JPG` },
   { name: 'SafZ',    handle: '@crypsaf',       url: `${BASE}/One/SafZ.JPG` },
-  { name: 'Jasich',  handle: '@thejasich',     url: `${BASE}/One/Jasich.JPG` },
-  { name: 'Kelvin',  handle: '@kelvinoyibo2',  url: `${BASE}/One/H0ld%20.JPG` },
-  { name: 'Tess',    handle: '@tessonchain',   url: `${BASE}/One/Tess.JPG` },
-  { name: 'Smart',   handle: '@xmartsol',      url: `${BASE}/One/Smart.JPG` },
   { name: 'Gorilla', handle: '@cryptogorilla', url: `${BASE}/One/Gorilla.JPG` },
   { name: 'JBond',   handle: '@jbondwagon',    url: `${BASE}/One/JBond.JPG` },
 ];
